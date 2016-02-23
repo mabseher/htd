419d28193ffc85a764a0de3e2ab94adb0a5edba4
Michael Abseher <abseher@dbai.tuwien.ac.at>
2016-02-23 11:13:24 +0100

    Updated configuration for Travis-CI.

--- travis.sh
+++ travis.sh
@@ -4,13 +4,15 @@ set -evx
 env | sort
 
 mkdir build || true
-mkdir build/$HTD_TARGET || true
-cd build/$HTD_TARGET
+mkdir build/${HTD_TARGET} || true
+cd build/${HTD_TARGET}
 
-cmake -DCMAKE_BUILD_TYPE=Debug \
+cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+      -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} \
+      -DHTD_USE_EXTENDED_IDENTIFIERS=${HTD_USE_EXTENDED_IDENTIFIERS} \
       -DHTD_BUILD_TESTS=True \
-      -DCMAKE_CXX_FLAGS=$CXX_FLAGS \
+      -DCMAKE_CXX_FLAGS=${CXX_FLAGS} \
       ../..
 
-make
+make VERBOSE=1
 make test

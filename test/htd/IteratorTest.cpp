71ae427fbc47395a0af1538afcafa7adc009599c
Michael Abseher <abseher@dbai.tuwien.ac.at>
2016-04-07 10:05:34 +0200

    Updated test cases for custom iterators.

--- test/htd/IteratorTest.cpp
+++ test/htd/IteratorTest.cpp
@@ -172,6 +172,89 @@ TEST(IteratorTest, CheckIteratorCloneMethod)
     delete it2;
 }
 
+TEST(IteratorTest, CheckConstCollectionWrapper1)
+{
+    std::vector<int> values { 1, 2, 3 };
+
+    htd::ConstCollection<int> collection1 = htd::ConstCollection<int>::getInstance(values);
+
+    ASSERT_FALSE(collection1.empty());
+    ASSERT_EQ((std::size_t)3, collection1.size());
+
+    auto it1 = collection1.begin();
+
+    ASSERT_TRUE(it1 == collection1.begin());
+    ASSERT_FALSE(it1 != collection1.begin());
+
+    for (htd::index_t index = 0; index < values.size(); ++index)
+    {
+        ASSERT_EQ(values[index], *it1);
+
+        ++it1;
+    }
+
+    ASSERT_TRUE(collection1.begin() == collection1.begin());
+    ASSERT_TRUE(collection1.end() == collection1.end());
+    ASSERT_TRUE(it1 == collection1.end());
+
+    ASSERT_FALSE(collection1.begin() != collection1.begin());
+    ASSERT_FALSE(collection1.end() != collection1.end());
+    ASSERT_FALSE(it1 != collection1.end());
+
+    auto begin = collection1.begin();
+    auto end = collection1.end();
+    auto beginPtr = reinterpret_cast<htd::ConstIteratorBase<int> *>(&begin);
+    auto endPtr = reinterpret_cast<htd::ConstIteratorBase<int> *>(&end);
+
+    ASSERT_TRUE(collection1.begin() == *beginPtr);
+    ASSERT_TRUE(collection1.end() == *endPtr);
+    ASSERT_TRUE(it1 == *endPtr);
+
+    ASSERT_FALSE(collection1.begin() != *beginPtr);
+    ASSERT_FALSE(collection1.end() != *endPtr);
+    ASSERT_FALSE(it1 != *endPtr);
+}
+
+TEST(IteratorTest, CheckConstCollectionWrapper2)
+{
+    std::vector<std::pair<int, int>> values { std::make_pair<int, int>(1, 2) };
+
+    htd::ConstCollection<std::pair<int, int>> collection1 = htd::ConstCollection<std::pair<int, int>>::getInstance(values);
+
+    ASSERT_FALSE(collection1.empty());
+    ASSERT_EQ((std::size_t)1, collection1.size());
+
+    auto it1 = collection1.begin();
+
+    ASSERT_TRUE(it1 == collection1.begin());
+
+    ASSERT_EQ(1, it1->first);
+    ASSERT_EQ(2, it1->second);
+
+    ++it1;
+
+    ASSERT_TRUE(it1 == collection1.end());
+}
+
+TEST(IteratorTest, CheckConstIteratorCloneMethod)
+{
+    std::vector<int> values { 1, 2, 3 };
+
+    htd::ConstCollection<int> collection1 = htd::ConstCollection<int>::getInstance(values);
+
+    auto it1 = collection1.begin();
+
+    ++it1;
+
+    auto it2 = it1.clone();
+
+    ASSERT_EQ(2, *(*it2));
+
+    ASSERT_TRUE(it1 == *it2);
+
+    delete it2;
+}
+
 int main(int argc, char **argv)
 {
     /* GoogleTest may throw. This results in a non-zero exit code and is intended. */
